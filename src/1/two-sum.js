c46e3e167ec6cbe18f7226cf619123a045cfc4ac
ZHIMIN ZHANG <nzhlfred@gmail.com>
2018-10-08 00:02:23 +0100

    AC problem-1

--- src/1/two-sum.js
+++ src/1/two-sum.js
@@ -4,11 +4,35 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; ++i) {
-    for (let j = i + 1; i < nums.length; ++j) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j]
+  // bf
+  // for (let i = 0; i < nums.length; ++i) {
+  //   for (let j = i + 1; j < nums.length; ++j) {
+  //     if (nums[i] + nums[j] === target) {
+  //       return [i, j]
+  //     }
+  //   }
+  // }
+
+  // quick sort, n * binary search
+
+  // hash table, n * 1
+  const hashTable = {}
+  for (let index = 0; index < nums.length; index++) {
+    const num = nums[index]
+
+    hashTable[num] = hashTable[num]
+      ? hashTable[num].concat(index) : [index]
+  }
+
+  for (let index = 0; index < nums.length; index++) {
+    const num = nums[index]
+    if (hashTable[target - num]) {
+      if (target - num === num) {
+        if (hashTable[num].length === 1) continue
+        // two num with the same value
+        return [hashTable[num][0], hashTable[num][1]]
       }
+      return [index, hashTable[target - num][0]]
     }
   }
 }
--- src/1/two-sum.test.js
+++ src/1/two-sum.test.js
@@ -1,5 +1,21 @@
 const solution = require('./two-sum.js')
 
-test('[2, 7, 11, 15]', () => {
+test('[2, 7, 11, 15] 9', () => {
   expect(solution([2, 7, 11, 15], 9)).toEqual([0, 1])
+})
+
+test('[2, 11, 15, 7] 9', () => {
+  expect(solution([2, 11, 15, 7], 9)).toEqual([0, 3])
+})
+
+test('[2, 7, 11, 15] 18', () => {
+  expect(solution([2, 7, 11, 15], 18)).toEqual([1, 2])
+})
+
+test('[2, 11, 11, 15] 22', () => {
+  expect(solution([2, 11, 11, 15], 22)).toEqual([1, 2])
+})
+
+test('[3, 2, 4] 6', () => {
+  expect(solution([3, 2, 4], 6)).toEqual([1, 2])
 })
