12210974782534658f3ef1b02f341bffccffc103
ZHIMIN ZHANG <nzhlfred@gmail.com>
2018-10-09 21:24:09 +0100

    AC problem-3

---
+++ src/3/longest-substring-without-repeating-characters.test.js
@@ -0,0 +1,26 @@
+const solution = require('./longest-substring-without-repeating-characters.js')
+
+let string = 'abcabcbb'
+test(string, () => {
+  expect(solution(string)).toEqual(3)
+})
+
+let string2 = 'bbbbb'
+test(string2, () => {
+  expect(solution(string2)).toEqual(1)
+})
+
+let string3 = 'pwwkew'
+test(string3, () => {
+  expect(solution(string3)).toEqual(3)
+})
+
+let string4 = ''
+test(string4, () => {
+  expect(solution(string4)).toEqual(0)
+})
+
+let string5 = 'a'
+test(string5, () => {
+  expect(solution(string5)).toEqual(1)
+})
