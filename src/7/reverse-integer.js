9dcd90649f5d9c34ba2dba0751c6b3196f44643c
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2018-12-20 19:06:04 +0000

    AC problem-7

--- src/7/reverse-integer.js
+++ src/7/reverse-integer.js
@@ -2,6 +2,21 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    
-};
+var reverse = function (x) {
+  const MIN = -Math.pow(2, 31)
+  const MAX = Math.pow(2, 31) - 1
+  const NEG = x < 0
+
+  // -403 % 10 = -3
+  // 403 % 10 = 3
+  let re = 0
+  while (x !== 0) {
+    re = re * 10 + x % 10
+    x = NEG ? Math.ceil(x / 10) : Math.floor(x / 10)
+    if (re < MIN || re > MAX) return 0
+  }
+
+  return re
+}
+
+module.exports = reverse
--- src/7/reverse-integer.test.js
+++ src/7/reverse-integer.test.js
@@ -1,1 +1,25 @@
 const solution = require('./reverse-integer.js')
+
+test('123', () => {
+  expect(solution(123)).toEqual(321)
+})
+
+test('2147483643', () => {
+  expect(solution(2147483643)).toEqual(0)
+})
+
+test('0', () => {
+  expect(solution(0)).toEqual(0)
+})
+
+test('-400', () => {
+  expect(solution(-400)).toEqual(-4)
+})
+
+test('-2147483648', () => {
+  expect(solution(-2147483648)).toEqual(0)
+})
+
+test('-2147483648', () => {
+  expect(solution(-2147483648)).toEqual(0)
+})
