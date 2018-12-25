9a5681176c8f8606668e719297fc50c216f4f8bd
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2018-12-25 20:05:39 +0000

    revised problem-1

--- src/1/two-sum.js
+++ src/1/two-sum.js
@@ -4,18 +4,8 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  // bf
-  // for (let i = 0; i < nums.length; ++i) {
-  //   for (let j = i + 1; j < nums.length; ++j) {
-  //     if (nums[i] + nums[j] === target) {
-  //       return [i, j]
-  //     }
-  //   }
-  // }
-
-  // quick sort, n * binary search
-
   // hash table, n * 1
+  // each entry is a list
   const hashTable = {}
   for (let index = 0; index < nums.length; index++) {
     const num = nums[index]
@@ -28,8 +18,11 @@ var twoSum = function (nums, target) {
     const num = nums[index]
     if (hashTable[target - num]) {
       if (target - num === num) {
+        // two num with the same value, but
+        // we only have one. I.e. can't use
+        // each num twice.
         if (hashTable[num].length === 1) continue
-        // two num with the same value
+
         return [hashTable[num][0], hashTable[num][1]]
       }
       return [index, hashTable[target - num][0]]
