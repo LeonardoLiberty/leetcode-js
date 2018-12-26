7bd3fddf439ecea429125b514f37bc56654909f1
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2018-12-26 11:53:15 +0000

    revised problem-2

--- src/2/add-two-numbers.js
+++ src/2/add-two-numbers.js
@@ -11,8 +11,30 @@
  * @return {ListNode}
  */
 
-// 1. 返回值实际是一个array, 这里对js非常不友好~
-// 2. 初始思路没有考虑到超大数字 i.e. 超出js能表示的数字
+// 1. 返回值实际是一个array, 这里实际上没有说清楚.
+// 2. 刚开始考虑的是将两个值转化为数字然后相加后再转回list
+//  + 这种思路缺陷在于没有考虑到超大数字 i.e. 超出js能表示的数字
+//  + 实际上由于两个链表的头都是个位数, 所以只需要按照加法规则往后算即可.
+
+var addTwoNumbers = function (l1, l2) {
+  let leftNode = l1
+  let rightNode = l2
+  let shift = 0
+  let array = []
+
+  while (leftNode || rightNode || shift) {
+    let leftVal = leftNode ? leftNode.val : 0
+    let rightVal = rightNode ? rightNode.val : 0
+    let val = (leftVal + rightVal + shift) % 10
+    shift = Math.floor((leftVal + rightVal + shift) / 10)
+    array.push(val)
+
+    leftNode = leftNode ? leftNode.next : null
+    rightNode = rightNode ? rightNode.next : null
+  }
+
+  return array
+}
 
 function list2num (list) {
   let num = 0
@@ -41,38 +63,6 @@ function num2list (num) {
   return head
 }
 
-// function list2array (list) {
-//   let curNode = list
-//   let array = []
-//   while (curNode) {
-//     array.push(curNode.val)
-//     curNode = curNode.next
-//   }
-//   return array
-// }
-
-// var addTwoNumbers = function (l1, l2) {
-//   return list2array(num2list(list2num(l1) + list2num(l2)))
-// }
-
-var addTwoNumbers = function (l1, l2) {
-  let leftNode = l1
-  let rightNode = l2
-  let shift = 0
-  let array = []
-
-  while (leftNode || rightNode || shift) {
-    let leftVal = leftNode ? leftNode.val : 0
-    let rightVal = rightNode ? rightNode.val : 0
-    let val = (leftVal + rightVal + shift) % 10
-    shift = Math.floor((leftVal + rightVal + shift) / 10)
-    array.push(val)
-
-    leftNode = leftNode ? leftNode.next : null
-    rightNode = rightNode ? rightNode.next : null
-  }
-
-  return array
-}
-
-module.exports = { list2num, num2list, addTwoNumbers }
+// 测试辅助函数
+export { list2num, num2list }
+export default addTwoNumbers
--- src/2/add-two-numbers.test.js
+++ src/2/add-two-numbers.test.js
@@ -1,19 +1,12 @@
-const { list2num, num2list, addTwoNumbers } =
-  require('./add-two-numbers.js')
-
-console.log(num2list(243))
-console.log(num2list(0))
-console.log(num2list(1000))
-console.log(num2list(99))
-console.log(list2num(num2list(1000)))
-console.log(list2num(num2list(0)))
+/* eslint-env jest */
+import solution, { num2list } from './add-two-numbers.js'
 
 test('243 564 807', () => {
-  expect(addTwoNumbers(num2list(243), num2list(564))).toEqual([7, 0, 8])
+  expect(solution(num2list(243), num2list(564))).toEqual([7, 0, 8])
 })
 
 test('999 1 1000', () => {
-  expect(addTwoNumbers(num2list(999), num2list(1))).toEqual([0, 0, 0, 1])
+  expect(solution(num2list(999), num2list(1))).toEqual([0, 0, 0, 1])
 })
 
 // without consider extremely long num
