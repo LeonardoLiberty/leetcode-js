9aca09938c714e5b7a77247d5a24e330132268b5
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2018-12-31 12:21:59 +0000

    revise problem-4

--- src/4/median-of-two-sorted-arrays.test.js
+++ src/4/median-of-two-sorted-arrays.test.js
@@ -1,4 +1,5 @@
-const solution = require('./median-of-two-sorted-arrays.js')
+/* eslint-env jest */
+import solution from './median-of-two-sorted-arrays.js'
 
 test('[1, 3] [2] 2', () => {
   expect(solution([1, 3], [2])).toEqual(2)
@@ -18,4 +19,12 @@ test('[] [1, 2, 3, 4] 2.5', () => {
 
 test('[] [2, 3, 4] 3', () => {
   expect(solution([], [2, 3, 4])).toEqual(3)
+})
+
+test('[1 2 3 4] [2, 3, 4] 3', () => {
+  expect(solution([1, 2, 3, 4], [2, 3, 4])).toEqual(3)
+})
+
+test('[1 2 3 ] [2, 3, 4] 2.5', () => {
+  expect(solution([1, 2, 3], [2, 3, 4])).toEqual(2.5)
 })
