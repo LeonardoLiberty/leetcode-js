0d5b1d71b215817b0306563027a8d30a10006b4a
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2019-01-16 13:59:17 +0000

    AC 替换空格

---
+++ src/剑指offer/替换空格/替换空格.js
@@ -0,0 +1,5 @@
+function replaceSpace (str) {
+  return str.replace(/ /g, '%20')
+}
+
+export default replaceSpace
---
+++ src/剑指offer/替换空格/替换空格.test.js
@@ -0,0 +1,6 @@
+/* eslint-env jest */
+import solution from './替换空格.js'
+
+test('test name', () => {
+  expect(solution(para)).toEqual()
+})
