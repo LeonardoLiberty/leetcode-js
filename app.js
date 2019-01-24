d4080b5ae299b49496e07934ba8abc2f62776693
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2019-01-24 21:14:17 +0000

    using promise to make code clean

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const readline = require('readline')
 const fsPromises = require('fs').promises
 
+function getReadlinePromises () {
+  const readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  function question (prompt) {
+    return new Promise((resolve, reject) => {
+      try {
+        readlineInterface.question(prompt, input => resolve(input))
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+
+  function close () { readlineInterface.close() }
+
+  return { question, close }
+}
+
 const initJZOffer = (name) => {
   return fsPromises.mkdir(`./src/剑指offer/${name}`)
     .then(() => {
-      Promise.all([
-        fsPromises.writeFile(
-          `./src/剑指offer/${name}/${name}.js`,
-          `\nexport default solution\n`
-        )
-        // fsPromises.writeFile(
-        //   `./src/剑指offer/${name}/${name}.test.js`,
-        //   // file template
-        //   `/* eslint-env jest */\nimport solution from './${name}.js'\n\ntest('test name', () => {\n  expect(solution(para)).toEqual()\n})\n`
-        // )
-      ])
+      return fsPromises.writeFile(
+        `./src/剑指offer/${name}/${name}.js`,
+        `\nexport default solution\n`
+      )
     })
     .then(() => {
       console.log(`\n\tcd ./src/剑指offer/${name} : go to problem directory.\n\n\tnpm run test: run the test.\n\nInitialization finishes and happy hacks >`)
@@ -26,17 +40,18 @@ const initJZOffer = (name) => {
 const initLeetCode = (id, url) => {
   const name = url.split('/')[4]
   fsPromises.mkdir(`./src/leetcode/${id}`).then(() => {
-    // fsPromises.open(`./src/leecode/${id}/${name}.js`, 'a')
-    fsPromises.writeFile(
-      `./src/leetcode/${id}/${name}.js`,
-      `\nexport default solution\n`
-    )
+    return Promise.all([
+      fsPromises.writeFile(
+        `./src/leetcode/${id}/${name}.js`,
+        `\nexport default solution\n`
+      ),
+      fsPromises.writeFile(
+        `./src/leetcode/${id}/${name}.test.js`,
+        // file template
+        `/* eslint-env jest */\nimport solution from './${name}.js'\n\ntest('test name', () => {\n  expect(solution(para)).toEqual()\n})\n`
+      )
 
-    fsPromises.writeFile(
-      `./src/leetcode/${id}/${name}.test.js`,
-      // file template
-      `/* eslint-env jest */\nimport solution from './${name}.js'\n\ntest('test name', () => {\n  expect(solution(para)).toEqual()\n})\n`
-    )
+    ])
   }).then(() => {
     console.log(`\n\tcd ./src/leetcode/${id} : go to problem directory.\n\n\tnpm run test: run the test.\n\nInitialization finishes and happy hacks >`)
   }).catch(error => {
@@ -46,40 +61,32 @@ const initLeetCode = (id, url) => {
 
 function choiceHandler (choice) {
   if (choice === '剑指offer') {
-    readlineInterface.question('\nProblem Name: ', name => {
-      readlineInterface.close()
-      initJZOffer(name)
-    })
+    return question('\nProblem Name: ')
+      .then(name => initJZOffer(name))
   } else {
     // leetcode
-    readlineInterface.question('\nProblem ID: ', id => {
-      readlineInterface.question('\nProblem Url: ', url => {
-        readlineInterface.close()
-        initLeetCode(id, url)
+    let leetcodeId
+    return question('\nProblem ID: ')
+      .then(id => {
+        leetcodeId = id
+        return question('\nProblem Url: ')
       })
-    })
+      .then(url => initLeetCode(leetcodeId, url))
   }
+}
+
+function PrintPrompt (choices) {
+  let prompt = ''
+  choices.forEach((choice, index) => {
+    prompt += `\n${index + 1}. ${choice}`
+  })
+  prompt += '\nYour choice: '
+  return question(prompt)
+    .then(index => choices[index - 1] && choiceHandler(choices[index - 1]))
 }
 
 // ------ start running  ---------
 
-let readlineInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-fsPromises.readdir('./src')
-  .then(choices => {
-    let prompt = ''
-    choices.forEach((choice, index) => {
-      prompt += `\n${index + 1}. ${choice}`
-    })
-    prompt += '\nYour choice: '
-    readlineInterface.question(prompt, index => {
-      if (!choices[index - 1]) {
-        readlineInterface.close()
-      } else {
-        return choiceHandler(choices[index - 1])
-      }
-    })
-  })
+const { question, close } = getReadlinePromises()
+const choices = ['leetcode', '剑指offer']
+PrintPrompt(choices).finally(() => close())
