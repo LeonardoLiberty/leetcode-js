f6e1067ff7d156ca86cd3afb160311c7ad3248d4
ZHIMIN ZHNAG <77595103@zhiminzhang.com>
2019-01-25 17:34:50 +0000

    AC 丑数

---
+++ src/剑指offer/丑数/丑数.js
@@ -0,0 +1,20 @@
+function GetUglyNumber_Solution (index) {
+  // 天坑边界
+  if (index <= 0) return 0
+  if (index === 1) return 1
+  let stack = [1]
+  let ptr2 = 0
+  let ptr3 = 0
+  let ptr5 = 0
+  for (let i = 2; i <= index; ++i) {
+    const minValue = Math.min(stack[ptr2] * 2, stack[ptr3] * 3, stack[ptr5] * 5)
+    if (minValue === stack[ptr2] * 2) ptr2++
+    if (minValue === stack[ptr3] * 3) ptr3++
+    if (minValue === stack[ptr5] * 5) ptr5++
+
+    stack.push(minValue)
+  }
+  return stack.pop()
+}
+
+export default GetUglyNumber_Solution
