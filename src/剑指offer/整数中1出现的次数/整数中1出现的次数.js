e7c3537c48b156becd595982df125f79bfcc4b2a
ZHIMIN ZHNAG <77595103@zhiminzhang.com>
2019-02-09 12:20:46 +0800

    review loop1: added commented for 整数中1出现的次数（从1到n整数中1出现的次数）

--- src/剑指offer/整数中1出现的次数/整数中1出现的次数.js
+++ src/剑指offer/整数中1出现的次数/整数中1出现的次数.js
@@ -1,23 +1,21 @@
 // http://www.cnblogs.com/cyjb/p/digitOccurrenceInRegion.html
 function NumberOf1Between1AndNSolution (n) {
-  if (n <= 0) return 0
-  let sum = 0
-
-  let i = 1
-  while (true) {
-    let temp = Math.floor(n / i)
-    if (temp === 0) break
-    sum += Math.floor(temp / 10) * i
-    if (temp % 10 > 1) {
-      sum += i
-    } else if (temp % 10 === 1) {
-      sum += n - temp * i + 1
+  let cnt = 0
+  let i = 1 // 1表示个位， 10表示十位， 100表示百位...
+  let times // 高位
+  let cur // 当前位
+  while (n / i >= 1) {
+    times = Math.floor(n / i / 10)
+    cnt += times * i
+    cur = Math.floor(n / i) % 10
+    if (cur > 1) {
+      cnt += i
+    } else if (cur === 1) {
+      cnt += (n % i) + 1
     }
-
     i *= 10
   }
-
-  return sum
+  return cnt
 }
 
 export default NumberOf1Between1AndNSolution
