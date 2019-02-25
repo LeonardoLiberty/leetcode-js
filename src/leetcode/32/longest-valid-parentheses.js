cb8b3b9d2ac92d117b3fe8faf3f811108468a9c9
ZHIMIN ZHNAG <77595103@zhiminzhang.com>
2019-02-25 09:02:58 +0800

    新加了一种解法

--- src/leetcode/32/longest-valid-parentheses.js
+++ src/leetcode/32/longest-valid-parentheses.js
@@ -3,6 +3,41 @@
  * @return {number}
  */
 var longestValidParentheses = function (s) {
+  let leftBra = 0
+  let rightBra = 0
+  let maxLen = 0
+
+  for (const each of s) {
+    if (each === '(') {
+      leftBra++
+    } else {
+      rightBra++
+      if (leftBra === rightBra) {
+        maxLen = maxLen > leftBra ? maxLen : leftBra
+      } else if (rightBra > leftBra) {
+        leftBra = 0
+        rightBra = 0
+      }
+    }
+  }
+
+  leftBra = 0
+  rightBra = 0
+  for (const each of s.split('').reverse().join('')) {
+    if (each === ')') {
+      rightBra++
+    } else {
+      leftBra++
+      if (leftBra === rightBra) {
+        maxLen = maxLen > leftBra ? maxLen : leftBra
+      } else if (leftBra > rightBra) {
+        leftBra = 0
+        rightBra = 0
+      }
+    }
+  }
+
+  return maxLen * 2
 }
 
 export default longestValidParentheses
